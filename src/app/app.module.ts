0d1066189248333c6a1ffb237a3818fc4e5ae242
Rajesh Rathore <rajeshrathore05011998@gmail.com>
2022-06-06 00:01:20 +0530

    feat:<Front view> front view added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { FrontComponent } from './components/front/front.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/front/front.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FrontComponent } from './front.component';
+
+describe('FrontComponent', () => {
+  let component: FrontComponent;
+  let fixture: ComponentFixture<FrontComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FrontComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FrontComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
